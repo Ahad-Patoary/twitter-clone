3a778d9a431a16cabae85414d011c553ea2180fa
Ahad-Patoary <ahad.patoary@thetravelnetworkgroup.co.uk>
2025-06-20 16:37:40 +0100

    Created User Model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+  userID: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  bio: string;
+  profileImage: string;
+  createdAt: string;
+}
